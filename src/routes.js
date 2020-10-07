552a838cef3cda6aad8942503d7f1e9eaa941623
wellingtoneugenio <wellingtoneugenio@gmail.com>
2020-10-07 15:10:11 -0300

    criacao usercontroler

--- src/routes.js
+++ src/routes.js
@@ -1,28 +1,10 @@
-const { Router } = require('express');
-
-import User from './app/models/User';
-
-const routes = new Router();
-
-routes.get('/', async (req,res)=>{
-
-  const user = await User.create({
-      name: 'Diego Fernandes',
-      email: 'diego@rocketseat.com.br',
-      password_hash:'123456',
-  });
-
-  return res.json(user);
-});
-
-export default routes;
-
+import { Router } from 'express';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World4' });
-});
+routes.post('/users', UserController.store);
 
 export default routes;
+
 
